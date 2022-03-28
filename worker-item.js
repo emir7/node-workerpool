769fcc6e8d36104a961dcef1c1f28f2b750b5a2d
emir7 <55662365+emir7@users.noreply.github.com>
2022-03-28 23:02:23 +0200

    Update worker-item.js

--- worker-item.js
+++ worker-item.js
@@ -1,21 +1,28 @@
 module.exports = class WorkerItem {
-    constructor(worker) {
-      this.worker = worker;
-      this.resolve = null;
-      this.reject = null;
-      this.isFree = true;
-    }
-  
-    scheduleTask(resolve, reject, data) {
-      this.resolve = resolve;
-      this.reject = reject;
-      this.worker.postMessage(data);
-      this.isFree = false;
-    }
-  
-    freeWorker() {
-      this.resolve = null;
-      this.reject = null;
-      this.isFree = true;
-    }
+  constructor(worker) {
+    this.worker = worker;
+    this.task = null;
+    this.isFree = true;
   }
+
+  scheduleTask(task) {
+    this.task = task;
+    this.worker.postMessage(task.data);
+    this.isFree = false;
+  }
+
+  freeWorker() {
+    this.task = null;
+    this.isFree = true;
+  }
+
+  onSuccess(data) {
+    this.task.resolve(data)
+    this.freeWorker();  
+  }
+
+  onError(err) {
+    this.task.reject(err);
+    this.freeWorker();
+  }
+}
